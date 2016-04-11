53e893f83ecf875916682b59422da24c1fb85178
Grant McWilliams <grant@soundlinuxtraining.com>
2016-04-10 23:29:29 -0700

    Fixes bugs in mkapp.sh so it updates the index.xml file

--- mkapp.sh
+++ mkapp.sh
@@ -36,7 +36,7 @@ if [[ -d ./app-files ]] ;then
 	cp -Rf plexee.plexforboxee app-files
 	cd app-files
 	if [[ -f plexee.plexforboxee/descriptor.xml ]] ;then
-		sed "s/<version>[0-9]\.[0-9]<\/version>/<version>$APPVERSION<\/version>/g" plexee.plexforboxee/descriptor.xml > ${TMPDIR}/descriptor.xml
+		sed "s/<version>[0-9]\.[0-9]*<\/version>/<version>${APPVERSION}<\/version>/g" plexee.plexforboxee/descriptor.xml > ${TMPDIR}/descriptor.xml
 	fi	
 	cat ${TMPDIR}/descriptor.xml
 	if yesno "Does this look OK" ;then
@@ -46,7 +46,13 @@ if [[ -d ./app-files ]] ;then
 		rm -f plexee.plexforboxee-${APPVERSION}.zip 
 	fi
 	zip -r "plexee.plexforboxee-${APPVERSION}.zip" plexee.plexforboxee
-        ln -s plexee.plexforboxee-${APPVERSION}.zip plexee.plexforboxee-latest.zip
+    if [[ -L plexee.plexforboxee-latest.zip ]];then
+		unlink plexee.plexforboxee-latest.zip
+	else
+		rm -f plexee.plexforboxee-latest.zip
+	fi
+
+    ln -s plexee.plexforboxee-${APPVERSION}.zip plexee.plexforboxee-latest.zip
 	echo "Upload and sign the zip file at http://bbx.boxee.tv/developer/appedit?appid=plexee.plexforboxee"
 	echo "then download the signature file to $PWD and resume this script"
 	read
@@ -62,22 +68,22 @@ if [[ -d ./app-files ]] ;then
 		echo "Unable to upload plexee.plexforboxee-${APPVERSION}.zip and signature to ${APPREPO} - exiting"
 		exit 1
 	fi
-	if ! scp ./index.xml "root@${APPREPO}:${APPPATH}/" ; then 
-		echo "Unable to upload new repository index - exiting"
-		exit 1
-	fi
 
 	if ! ssh root@${APPREPO} "cd ${APPPATH}/download/; unlink plexee.plexforboxee-latest.zip; ln -s plexee.plexforboxee-${APPVERSION}.zip plexee.plexforboxee-latest.zip; chown -R apache.apache ${APPPATH}/; chmod -R 755 ${APPPATH}/; restorecon ${APPPATH}/" ;then
 		echo "Unable to change permissions on ${APPPATH}"
 	fi
 
 	if [[ ! -e ./index.xml ]] ;then
-		if scp root@${APPREPO}:${APPPATH}/index.xml ./index.xml ; then 
-	else
-		echo "Unable to download ${APPPATH}/index.xml -exiting"
-		exit 1
+		if ! scp root@${APPREPO}:${APPPATH}/index.xml ./index.xml ; then 
+			echo "Unable to download ${APPPATH}/index.xml -exiting"
+			exit 1
+		fi
 	fi
 	
-	sed -i "s/<version>[0-9]\.[0-9][0-9]?<\/version>/<version>${APPVERSION}<\/version>/g" ./index.xml 
-	sed -i "s/(v. [0-9]\.[0-9][0-9]?)/(v. ${APPVERSION})/g" ./index.xml 
+	sed -i "s/<version>[0-9]\.[0-9][0-9]*<\/version>/<version>${APPVERSION}<\/version>/g" ./index.xml 
+	sed -i "s/(v. [0-9]\.[0-9][0-9]*)/(v. ${APPVERSION})/g" ./index.xml 
+	if ! scp ./index.xml "root@${APPREPO}:${APPPATH}/" ; then 
+		echo "Unable to upload new repository index - exiting"
+		exit 1
+	fi
 fi
